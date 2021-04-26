2de4a9f771f537c5b2e6a90c3357b7802d30de8d
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-04-26 18:33:00 +0430

    update input form highothercomponent

--- src/common/form/ui/date.form.jsx
+++ src/common/form/ui/date.form.jsx
@@ -1,24 +1,50 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import momentJalaali from "moment-jalaali";
 import DatePicker from "react-datepicker2";
+import { InputForm } from ".";
+const DateForm = ({
+  placeholder,
+  inputJalaaliFormat = "",
+  isDate,
+  isTime,
+  isAPM,
+  ...props
+}) => {
+  const [state, setState] = useState();
+  const ref = useRef();
+  useEffect(() => {
+    if (!ref || !ref.current) return;
 
-export default class component extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: momentJalaali(),
-      isGregorian: false,
-    };
-  }
-  render() {
-    return (
-      <div>
-        <DatePicker
-          value={this.state.value}
-          isGregorian={this.state.isGregorian}
-          onChange={(value) => this.setState({ value })}
-        />
-      </div>
-    );
-  }
-}
+    ref.current.input.placeholder = placeholder;
+  }, [ref]);
+  const onDateChange = (value) => {
+    // momentJalaali()
+    setState(value);
+  };
+  let jformat = inputJalaaliFormat;
+  if (isDate) jformat += "YYYY/MM/DD";
+  if (isTime) jformat += jformat ? " " : "" + "hh:mm:ss";
+  if (isAPM) jformat += jformat ? " " : "" + "a";
+
+  return (
+    <InputForm
+      {...props}
+      render={() => (
+        <div className="form-control form-panel-input">
+          <DatePicker
+            ref={ref}
+            persianDigits={true}
+            className="w-100"
+            isGregorian={false}
+            value={state}
+            onChange={onDateChange}
+            inputJalaaliFormat={jformat}
+            
+          />
+        </div>
+      )}
+    />
+  );
+};
+
+export default DateForm;
