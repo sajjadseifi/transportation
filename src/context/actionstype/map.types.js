6a350bbd6763ca228d91267a8bc2562afb7dbca1
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-06-20 12:02:50 +0430

    build:actionTypes

--- src/context/actionstype/map.types.js
+++
@@ -1,5 +0,0 @@
-export const INITIAL_STATE = 'INITIAL_STATE';
-export const UPDATE_STATE = 'UPDATE_STATE';
-export const CLEAR_STATE = 'CLEAR_STATE';
-export const SET_OPTIONS = 'SET_OPTIONS';
-export const SET_USER = 'SET_USER';
