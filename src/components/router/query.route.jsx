1a2b49da88bc266867dd1715502ce5dff1585753
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-06-20 04:47:25 +0430

    add:feature(override null value)

--- src/components/router/query.route.jsx
+++ src/components/router/query.route.jsx
@@ -1,20 +1,29 @@
 //default
 import React from 'react'
 import queryString from 'query-string'
-import { Route } from 'react-router-dom'
+import { Route, useHistory } from 'react-router-dom'
 export const QueryRoute = ({
   queryList = [] || '',
+  overideNullVAue = {},
   component: Component,
   ...rest
 }) => {
   let params = {}
-
+  let reChange = false
+  const history = useHistory()
   const qs = queryString.parse(rest.location.search)
-
   const qls = typeof queryList === 'string' ? queryList.split(',') : queryList
-  qls.forEach((q) => (params[q] = qs[q]))
+  qls.forEach((q) => {
+    params[q] = qs[q] ? qs[q] : overideNullVAue[q]
+    if (!qs[q] && overideNullVAue[q]) reChange = true
+  })
+
+  if (reChange) {
+    const search = queryString.stringify(params, {})
+    history.push(`${history.location.pathname}?${search}`)
+    return null
+  }
 
   rest.location.state = { ...params }
-
   return <Route {...{ rest }} component={Component} />
 }
