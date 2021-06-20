e7dcd48047d68d579a6db0f05476ec03443d0266
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-06-20 08:00:08 +0430

    build:feacher(exported)

---
+++ src/components/mapir/index.js
@@ -0,0 +1,5 @@
+import Mapir from 'mapir-react-component'
+export { MapOverview } from "./map.overview"
+export { MapMarking } from "./map.marking"
+export { MapDevelopment } from "./map.development"
+export { Mapir }
