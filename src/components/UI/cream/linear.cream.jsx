c651f1cafa98da9744c32e48f9f6bc847f23e6a5
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-06-22 01:44:27 +0430

    build:cream(lieanr)

--- src/components/UI/cream/linear.cream.jsx
+++ src/components/UI/cream/linear.cream.jsx
@@ -1,5 +1,45 @@
 //default
 import React from 'react'
-export const LinearCream = ({}) => {
-  return <div></div>
+import { persent } from '../../../core/utils/utils'
+import { NinjaConditionWrappr as NinWrap } from '../../wrapper'
+import classes from './linear.cream.module.css'
+const defTranition = 0.1
+export const LinearCream = ({
+  currentStep = 1,
+  sizeStep = 1,
+  vertical = false,
+  customCream = null,
+  speed = 2,
+  basebg = '#fff',
+  creambg = '#0084ff',
+}) => {
+  const transition = speed * defTranition
+  let creamSize = 0
+  let posPlace = 0
+
+  console.log({})
+  if (currentStep >= 0 && currentStep < sizeStep) {
+    creamSize = persent(100 / sizeStep)
+    posPlace = persent(((currentStep % sizeStep) * 100) / sizeStep)
+  }
+
+  const pos = vertical ? 'top' : 'left'
+  const size = vertical ? 'height' : 'width'
+
+  const creamStyle = {
+    [pos]: posPlace,
+    [size]: creamSize,
+    transition,
+  }
+
+  return (
+    <div style={{ background: basebg }} className={classes.LinearCream}>
+      <div style={creamStyle} className={classes.Cream}>
+        <NinWrap condition={!!customCream}>
+          {customCream}
+          <div style={{ background: creambg }} className={classes.Body}></div>
+        </NinWrap>
+      </div>
+    </div>
+  )
 }
