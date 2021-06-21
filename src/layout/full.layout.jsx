044ac8238e24fd7548b1e1e670fe2b7b145acfed
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-06-21 08:20:44 +0430

    fixed:responsive(with useWindowSize hook)

--- src/layout/full.layout.jsx
+++ src/layout/full.layout.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Sidebar } from './menu'
 import './layout.css'
 import { HeaderLayout } from './header'
 import { useSecurity } from '../core/security'
 import { roleType } from '../constants'
 import { ConentLayout } from './content/content.layout'
+import { useWindowSize } from '../core/hook'
+import { useSelector } from 'react-redux'
 const FullLayout = ({ children }) => {
   const { state } = useSecurity()
+  const activeSidebar = useSelector((state) => state.menu.activeSidebar)
+  const sidebarRef = useRef()
+  const [width, height] = useWindowSize()
+
   const appearance =
     state.user.role === roleType.CUSTOMER ? 'inverse' : 'default'
 
+  const contentWith = !activeSidebar ? width : width - 340
   return (
     <>
       <HeaderLayout />
       <div className="d-flex h-100">
-        <Sidebar appearance={appearance} />
-        <ConentLayout>{children}</ConentLayout>
+        <Sidebar ref={sidebarRef} appearance={appearance} />
+        <ConentLayout width={contentWith}>{children}</ConentLayout>
       </div>
     </>
   )
