110d9ffb37035fab081b0ddcdb79b3d3f9ad87e7
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-06-21 08:18:05 +0430

    build:map-card(+responsive)

---
+++ src/components/card/map.card.jsx
@@ -0,0 +1,6 @@
+//default
+import React from 'react'
+import classes from './map.card.module.css'
+export const MapCard = ({ children }) => {
+  return <div className={classes.MapCard}>{children}</div>
+}
