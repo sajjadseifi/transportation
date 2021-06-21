3e5cf2a9e376af98f02b90b89fc2db3aeb31b82a
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-06-21 08:25:48 +0430

    build:endpoint(route)

---
+++ src/core/api/endpoint.js
@@ -0,0 +1,10 @@
+export const endPoint = {
+   SavedAddress: "/savedaddress",
+   User: "/user",
+   UserIno: "/user-info",
+   Discount: "/discount",
+   Car: "/car",
+   Order: "/order",
+   RegionPrice: "/regionprice",
+   Ticket: "/ticket"
+}
