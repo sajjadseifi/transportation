01fe9c27b049ba9a9ac44402c13344cd4216c2bc
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-06-21 21:44:19 +0430

    fixed:map-provier(send to layout)

--- src/components/mapir/mapir.jsx
+++ src/components/mapir/mapir.jsx
@@ -1,19 +1,16 @@
 //default
 import React from 'react'
 import { MapDevelopment } from '.'
-import { MapProvider } from '../../context'
 import { SeachInputMapIr } from './search.mapir'
 export const MapIr = ({ disabled = false, search = null }) => {
   return (
-    <MapProvider>
-      <MapDevelopment
-        disabled={disabled}
-        search={search}
-        searchInput={SeachInputMapIr}
-        inputProps={{
-          placeholder: 'مثال:مازندران قائمشهر روستای باغدشت',
-        }}
-      />
-    </MapProvider>
+    <MapDevelopment
+      disabled={disabled}
+      search={search}
+      searchInput={SeachInputMapIr}
+      inputProps={{
+        placeholder: 'مثال:مازندران قائمشهر روستای باغدشت',
+      }}
+    />
   )
 }
--- src/layout/content/map.layout.jsx
+++ src/layout/content/map.layout.jsx
@@ -1,11 +1,15 @@
 //default
 import React from 'react'
 import { MapIr } from '../../components/mapir'
+import { MapProvider } from '../../context'
+
 export const MapLayout = ({ disabled, children }) => {
   return (
-    <div className="map-layout">
-      <MapIr disabled={disabled} />
-      {children && <div className="map-layout-overlab">{children}</div>}
-    </div>
+    <MapProvider>
+      <div className="map-layout">
+        <MapIr disabled={disabled} />
+        {children && <div className="map-layout-overlab">{children}</div>}
+      </div>
+    </MapProvider>
   )
 }
