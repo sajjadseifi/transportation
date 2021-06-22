a7649ae74c35ca13bb60886aac961b581de79263
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-06-22 11:49:41 +0430

    fixed:issue(max call for changing props)

--- src/context/provider/form-map.privider.jsx
+++ src/context/provider/form-map.privider.jsx
@@ -1,17 +1,26 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { formMapInitialState, formMapRecuer } from '../reducer/form-map.reducer'
+import { loadMarkList } from '../actions/form-map.action'
 export const FormMapStateContext = React.createContext()
 
 export const FormMapProvider = ({
   children,
-  updateProps = false,
+  markerList = [],
+  updateWithProps = false,
   ...initialValues
 }) => {
-  const [state, dispatch] = useReducer(formMapRecuer, formMapInitialState)
+  const [state, dispatch] = useReducer(formMapRecuer, {
+    formMapInitialState,
+    markerList,
+  })
 
+  useEffect(() => {
+    dispatch(loadMarkList(markerList))
+    return () => dispatch(loadMarkList([]))
+  }, [markerList, dispatch])
   const prodState = Object.assign(state, initialValues)
-  const value = { state: prodState, dispatch }
 
+  const value = { state: prodState, dispatch }
   return (
     <FormMapStateContext.Provider value={value}>
       {children}
