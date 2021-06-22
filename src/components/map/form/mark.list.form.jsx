165140712cb94ed8d4c47c37a9b1a2f49e3b4936
sajjad seifi <6482249-sajjadseifi@users.noreply.gitlab.com>
2021-06-22 06:50:03 +0430

    add:feacher(move on map with click)📍

--- src/components/map/form/mark.list.form.jsx
+++ src/components/map/form/mark.list.form.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
 import { MarkItemForm } from './mark.item.form'
 import classes from './form.map.module.css'
-export const MarkListForm = ({ marks = [], active, onSelect }) => (
-  <ul className={classes.MarkListForm}>
-    {marks.map((mk, index) => (
-      <li className={classes.MarkListItemForm}>
-        {console.log({ active, key: mk.key }, `${active}` === `${mk.key}`)}
-        <MarkItemForm
-          actived={`${active}` === `${mk.key}`}
-          onSelect={onSelect}
-          {...mk}
-          key={index}
-        />
-      </li>
-    ))}
-  </ul>
-)
+import {
+  useFormMapDispatch,
+  useFormMapSeletor,
+} from '../../../context/hook/form-map.hook'
+import * as formMapActions from '../../../context/actions/form-map.action'
+import { setCenter } from '../../../context/actions/map.action'
+import { useMapDispatch, useMapSelector } from '../../../context/hook/map.hook'
+
+export const MarkListForm = ({ useMap }) => {
+  const dispatch = useFormMapDispatch()
+  const dispatchMap = useMapDispatch()
+  const { markerList, onSelect, activeMark } = useFormMapSeletor()
+  const marks = useMapSelector((state) => state.marks)
+  const onSelectMark = (mark, index) => {
+    dispatch(formMapActions.activeMark(mark, index))
+    onSelect(mark, index)
+    console.log(`${useMap} && ${marks[mark.key]}`, useMap && marks[mark.key])
+    if (useMap && marks[mark.key]) dispatchMap(setCenter(marks[mark.key]))
+  }
+
+  return (
+    <ul className={classes.MarkListForm}>
+      {markerList.map((mk, index) => (
+        <li className={classes.MarkListItemForm}>
+          <MarkItemForm
+            actived={activeMark && `${activeMark.key}` === `${mk.key}`}
+            onSelect={() => onSelectMark(mk, index)}
+            {...mk}
+            mkey={mk.key}
+            key={index}
+          />
+        </li>
+      ))}
+    </ul>
+  )
+}
